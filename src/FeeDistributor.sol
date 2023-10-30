528c9e7fd95e6e7c98e424236a430449ea93caec
kphed <kpcrypto69@gmail.com>
2023-10-30 16:19:11 -0400

    Replace solady with solmate, inherit from FlywheelCore with placeholders

--- src/FeeDistributor.sol
+++ src/FeeDistributor.sol
@@ -1,4 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.10;
 
-contract FeeDistributor {}
+import {FlywheelCore} from "flywheel-v2/FlywheelCore.sol";
+import {IFlywheelRewards} from "flywheel-v2/interfaces/IFlywheelRewards.sol";
+import {IFlywheelBooster} from "flywheel-v2/interfaces/IFlywheelBooster.sol";
+import {FlywheelCore} from "flywheel-v2/FlywheelCore.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {Authority} from "solmate/auth/Auth.sol";
+
+contract FeeDistributor is FlywheelCore {
+    ERC20 private constant _WETH =
+        ERC20(0x4200000000000000000000000000000000000006);
+
+    constructor(
+        address _owner
+    )
+        FlywheelCore(
+            _WETH,
+            IFlywheelRewards(address(0)),
+            IFlywheelBooster(address(0)),
+            _owner,
+            Authority(address(0))
+        )
+    {}
+}
