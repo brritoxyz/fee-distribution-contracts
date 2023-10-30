07520da875048c7f035fd31cde03da336abb7af1
kphed <kpcrypto69@gmail.com>
2023-10-30 16:02:15 -0400

    Rename contracts to FeeDistributro, add FlywheelV2 remapping

---
+++ test/FeeDistributor.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+import {FeeDistributor} from "src/FeeDistributor.sol";
+
+contract FeeDistributorTest is Test {}
