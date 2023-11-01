50e42b25a62a84827756e8a82187fa17171c41cf
kphed <kpcrypto69@gmail.com>
2023-11-01 11:36:56 -0400

    Fix constructor by removing param, add state var init assertions

--- test/FeeDistributor.t.sol
+++ test/FeeDistributor.t.sol
@@ -7,6 +7,7 @@ import {FlywheelDynamicRewards} from "flywheel-v2/rewards/FlywheelDynamicRewards
 import {FeeDistributor} from "src/FeeDistributor.sol";
 import {StakedBRR} from "src/StakedBRR.sol";
 import {DynamicRewards} from "src/DynamicRewards.sol";
+import {RewardsStore} from "src/RewardsStore.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
 contract FeeDistributorTest is Test {
@@ -16,16 +17,43 @@ contract FeeDistributorTest is Test {
     FeeDistributor public immutable distributor;
     StakedBRR public immutable stakedBRR;
     DynamicRewards public immutable dynamicRewards;
+    RewardsStore public immutable dynamicRewardsStore;
 
-    constructor(address _rewardToken) {
+    constructor() {
         distributor = new FeeDistributor(WETH, owner, Authority(address(0)));
         stakedBRR = new StakedBRR(address(distributor));
         dynamicRewards = new DynamicRewards(
-            _rewardToken,
+            WETH,
             distributor,
             REWARDS_CYCLE_LENGTH
         );
+        dynamicRewardsStore = dynamicRewards.rewardsStore();
 
         distributor.setFlywheelRewards(dynamicRewards);
+        distributor.addStrategyForRewards(ERC20(address(stakedBRR)));
+
+        assertEq(
+            address(dynamicRewards),
+            address(distributor.flywheelRewards())
+        );
+        assertEq(owner, distributor.owner());
+
+        (uint224 index, uint32 lastUpdatedTimestamp) = distributor
+            .strategyState(ERC20(address(stakedBRR)));
+
+        assertEq(index, distributor.ONE());
+        assertEq(lastUpdatedTimestamp, block.timestamp);
+        assertEq(address(distributor), address(stakedBRR.flywheel()));
+        assertEq(address(distributor), address(dynamicRewards.flywheel()));
+        assertEq(REWARDS_CYCLE_LENGTH, dynamicRewards.rewardsCycleLength());
+        assertEq(WETH, dynamicRewardsStore.rewardToken());
+        assertEq(
+            address(dynamicRewards),
+            dynamicRewardsStore.flywheelRewards()
+        );
+
+        deal(WETH, address(this), 100 ether);
     }
+
+    function testTest() external {}
 }
