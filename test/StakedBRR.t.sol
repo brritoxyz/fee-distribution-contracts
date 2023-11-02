b715d6003ea5ba5a8c46876a684ea4ece3d77c4e
kphed <kpcrypto69@gmail.com>
2023-11-02 08:15:42 -0400

    Add stake tests

---
+++ test/StakedBRR.t.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "forge-std/Test.sol";
+import {ERC20} from "solady/tokens/ERC20.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
+import {StakedBRR} from "src/StakedBRR.sol";
+import {MockFlywheelCore} from "test/mocks/MockFlywheelCore.sol";
+
+contract StakedBRRTest is Test {
+    using SafeTransferLib for address;
+
+    address public constant BRR = 0x6d80d90ce251985bF41A98c6FDd6b7b975Fff884;
+    MockFlywheelCore public immutable flywheel = new MockFlywheelCore();
+    StakedBRR public immutable stakedBRR;
+
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    constructor() {
+        stakedBRR = new StakedBRR(address(flywheel));
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             stake
+    //////////////////////////////////////////////////////////////*/
+
+    function testCannotStakeInvalidTo() external {
+        vm.expectRevert(StakedBRR.InvalidTo.selector);
+
+        stakedBRR.stake(address(0), 1);
+    }
+
+    function testStake() external {
+        address msgSender = address(this);
+        address to = address(1);
+        uint256 amount = 1;
+
+        deal(BRR, msgSender, amount);
+
+        uint256 msgSenderBRRBalance = BRR.balanceOf(msgSender);
+        uint256 toStakedBRRBalance = stakedBRR.balanceOf(to);
+        uint256 flywheelCounter = flywheel.counter();
+
+        BRR.safeApprove(address(stakedBRR), amount);
+
+        vm.expectEmit(true, true, false, true, BRR);
+
+        emit Transfer(msgSender, address(stakedBRR), amount);
+
+        vm.expectEmit(true, true, false, true, address(stakedBRR));
+
+        emit Transfer(address(0), to, amount);
+
+        stakedBRR.stake(to, amount);
+
+        assertEq(msgSenderBRRBalance - amount, BRR.balanceOf(msgSender));
+        assertEq(toStakedBRRBalance + amount, stakedBRR.balanceOf(to));
+        assertEq(flywheelCounter + 1, flywheel.counter());
+
+        (ERC20 strategy, address user) = flywheel.accrueCall();
+
+        assertEq(address(stakedBRR), address(strategy));
+        assertEq(to, user);
+    }
+
+    function testStakeFuzz(
+        address msgSender,
+        address to,
+        uint256 amount
+    ) external {
+        vm.assume(to != address(0));
+
+        deal(BRR, msgSender, amount);
+
+        uint256 msgSenderBRRBalance = BRR.balanceOf(msgSender);
+        uint256 toStakedBRRBalance = stakedBRR.balanceOf(to);
+        uint256 flywheelCounter = flywheel.counter();
+
+        vm.startPrank(msgSender);
+
+        BRR.safeApprove(address(stakedBRR), amount);
+
+        vm.expectEmit(true, true, false, true, BRR);
+
+        emit Transfer(msgSender, address(stakedBRR), amount);
+
+        vm.expectEmit(true, true, false, true, address(stakedBRR));
+
+        emit Transfer(address(0), to, amount);
+
+        stakedBRR.stake(to, amount);
+
+        vm.stopPrank();
+
+        assertEq(msgSenderBRRBalance - amount, BRR.balanceOf(msgSender));
+        assertEq(toStakedBRRBalance + amount, stakedBRR.balanceOf(to));
+        assertEq(flywheelCounter + 1, flywheel.counter());
+
+        (ERC20 strategy, address user) = flywheel.accrueCall();
+
+        assertEq(address(stakedBRR), address(strategy));
+        assertEq(to, user);
+    }
+}
---
+++ test/mocks/MockFlywheelCore.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import {ERC20} from "solady/tokens/ERC20.sol";
+import {IFlywheelCore} from "src/interfaces/IFlywheelCore.sol";
+
+contract MockFlywheelCore is IFlywheelCore {
+    struct AccrueCall {
+        ERC20 strategy;
+        address user;
+    }
+
+    AccrueCall public accrueCall;
+    uint256 public counter;
+
+    function accrue(ERC20 strategy, address user) external returns (uint256) {
+        accrueCall = AccrueCall(strategy, user);
+
+        return ++counter;
+    }
+}
